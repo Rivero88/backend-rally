6dca3bc0eaf8cccf9b5660eb0adfe0072ab8f50b
Rivero88 <127329709+Rivero88@users.noreply.github.com>
2025-05-20 09:34:26 +0200

    Update SecurityConfig.java

--- src/main/java/com/rally/backend_rally/config/SecurityConfig.java
+++ src/main/java/com/rally/backend_rally/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://backend-rally-production-a980.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Si necesitas enviar cookies o cabeceras como Authorization

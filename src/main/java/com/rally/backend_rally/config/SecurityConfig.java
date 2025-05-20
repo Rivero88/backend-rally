bae343616093e4169101c93e7c5e206c5d7eec94
Rivero88 <127329709+Rivero88@users.noreply.github.com>
2025-05-20 17:14:13 +0200

    Modificacion securityConfig

--- src/main/java/com/rally/backend_rally/config/SecurityConfig.java
+++ src/main/java/com/rally/backend_rally/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.rally.backend_rally.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.rally.backend_rally.services.CustomUserDetailsService;
 

e216115e5ca1a8e48fc9f31ffd990ad4d54f9167
Rivero88 <127329709+Rivero88@users.noreply.github.com>
2025-05-20 18:25:02 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO rallydb.usuarios (alias, apellidos, email, f_nacimiento, nombre, num_telefono, password, rol)
 SELECT 'admin1', 'Gonzalez Romero', 'admin1@gmail.com','1988/09/19', 'Alejo', '698547856', '$2a$10$3smbmFlYsjdViYGMn6kEZeQ.kTIVWJiKu1uY7.a5QTTE0.ylg2HDK', 'admin'
-WHERE NOT EXISTS (SELECT 1 FROM usuario WHERE username = 'admin');
+WHERE NOT EXISTS (SELECT 1 FROM usuario WHERE alias = 'admin1');
